ece40b41234153ce9db70889ab5e07f74250a1f4
Kayla H <kaylajill12@gmail.com>
2025-06-26 15:38:12 -0400

    Added random background image fetch request

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,24 @@ const elements = {
     author: document.getElementById("author"),
 };
 
-const quotes = [
+async function getRandomImage() {
+    const client_id = "";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+getRandomImage();
+
+/* const quotes = [
     {
         quote: "All hands! Abandon ship!",
         author: "Captain Picard",
@@ -45,4 +62,4 @@ function loopThroughQuotes() {
     }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
